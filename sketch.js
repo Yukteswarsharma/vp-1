00dd5881ddd442be032ae8113daac56071e8e331
Yukteswarsharma <70581298+Yukteswarsharma@users.noreply.github.com>
2020-10-15 19:33:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,21 +1,21 @@
 //Create variables here
 var dog, happydog,  foods, foodStock;
 var database;
-var dogImg,happydogImg;
+var dog_Img,happydog_Img;
 
 
 function preload()
 {
   //load images here
-  this.image=loadImage('dogImg.png');
-  this.image=loadImage('happydog.png');
+  dog_Img=loadImage('dogImg.png');
+  happydog_Img=loadImage('happydog.png');
 }
 
 function setup() {
   createCanvas(500, 500);
   database = firebase.database();
   dog = createSprite(250, 250, 15, 5);
-  dog.addImage(dogImg);
+  dog.addImage(dog_Img);
   dog.scale=0.2
   foodStock=database.ref('food').on("value",readStock);
 }
@@ -27,10 +27,10 @@ background(46, 139, 87);
 
 if(keyWentDown(UP_ARROW)){
   writeStock(foods);
-  dog.addImage(happydogImg)
+  dog.addImage(happydog_Img)
 }
 else{
-  dog.addImage(dogImg)
+  dog.addImage(dog_Img)
 }
 
   drawSprites();
